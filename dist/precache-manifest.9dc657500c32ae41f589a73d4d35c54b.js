90ce3197278b20ea7d76bcb53147149e4468dd77
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-06-30 10:57:03 +0700

    release uat

---
+++ dist/precache-manifest.9dc657500c32ae41f589a73d4d35c54b.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660099d8808b2a57cd02",
+    "url": "/css/app.aa382206.css"
+  },
+  {
+    "revision": "4f7c443c07289cbf37de",
+    "url": "/css/chunk-vendors.8f9036e9.css"
+  },
+  {
+    "revision": "45c30ba15f0823349af666a0b1ab49d3",
+    "url": "/fonts/prompt-light-webfont.45c30ba1.ttf"
+  },
+  {
+    "revision": "a057d8bcfc0e54f96761adfb06bc4694",
+    "url": "/fonts/prompt-light-webfont.a057d8bc.woff2"
+  },
+  {
+    "revision": "cfac56fb162c87147269183ca6f90982",
+    "url": "/fonts/prompt-light-webfont.cfac56fb.woff"
+  },
+  {
+    "revision": "e80cc84f5996faf62d99bb26e5af37b8",
+    "url": "/fonts/prompt-light-webfont.e80cc84f.eot"
+  },
+  {
+    "revision": "382b38215397f16d3a47de164d066924",
+    "url": "/fonts/prompt-medium-webfont.382b3821.ttf"
+  },
+  {
+    "revision": "bb2fa0f2e3a39114640490547f1d365f",
+    "url": "/fonts/prompt-medium-webfont.bb2fa0f2.woff"
+  },
+  {
+    "revision": "e3a2d1b7cf5349c9fcb25243d33b6aa7",
+    "url": "/fonts/prompt-medium-webfont.e3a2d1b7.eot"
+  },
+  {
+    "revision": "fdd9b242d2050faedda76d99d5115362",
+    "url": "/fonts/prompt-medium-webfont.fdd9b242.woff2"
+  },
+  {
+    "revision": "464e8eb971b86445d5c3f95c6060825c",
+    "url": "/fonts/prompt-regular-webfont.464e8eb9.woff"
+  },
+  {
+    "revision": "63e535025f60e59ead811f96f48eeefa",
+    "url": "/fonts/prompt-regular-webfont.63e53502.woff2"
+  },
+  {
+    "revision": "8ea7866ac622a6c3acb3b78d5a119d84",
+    "url": "/fonts/prompt-regular-webfont.8ea7866a.ttf"
+  },
+  {
+    "revision": "add2db7b5215cb74c99fd1b9eb35331e",
+    "url": "/fonts/prompt-regular-webfont.add2db7b.eot"
+  },
+  {
+    "revision": "c648981719669b712f47a814ee36c67c",
+    "url": "/img/Collar.c6489817.svg"
+  },
+  {
+    "revision": "86dcd7e7fc4528e7e45e885bf34c8993",
+    "url": "/img/Community.86dcd7e7.svg"
+  },
+  {
+    "revision": "dc5ba07d342872c1a54f8e5fb1b27a89",
+    "url": "/img/Community.dc5ba07d.svg"
+  },
+  {
+    "revision": "848afe5fd160fc921b05844fc16f54eb",
+    "url": "/img/Feature.848afe5f.svg"
+  },
+  {
+    "revision": "ff91fccb876ea44da466bdf254b91cce",
+    "url": "/img/Home.ff91fccb.svg"
+  },
+  {
+    "revision": "f1a29c7e4bb5c7442ff58929d156586b",
+    "url": "/img/Normal.f1a29c7e.svg"
+  },
+  {
+    "revision": "4915f2d5555fe63a08e38fcb092bb36b",
+    "url": "/img/Phone.4915f2d5.svg"
+  },
+  {
+    "revision": "15b46b574beaea25db1c3ed0ca1cd76e",
+    "url": "/img/Place.15b46b57.svg"
+  },
+  {
+    "revision": "a902ededac0f19c11a21c6d0c0e52773",
+    "url": "/img/Store.a902eded.svg"
+  },
+  {
+    "revision": "440e500c3981e37530306d33d2f95af0",
+    "url": "/img/Technology.440e500c.svg"
+  },
+  {
+    "revision": "7336b5df6e022a3c80da7a67b523bd5c",
+    "url": "/img/Timeline.7336b5df.svg"
+  },
+  {
+    "revision": "cc4ebbec67775d8a8d3f377a60fc14e1",
+    "url": "/img/bone.cc4ebbec.png"
+  },
+  {
+    "revision": "5bb52aaf8fda83e0ed35f25825b534d5",
+    "url": "/img/deviceScan.5bb52aaf.png"
+  },
+  {
+    "revision": "e657724a4427d890d825d03c926d6a90",
+    "url": "/img/icon_PETDY.e657724a.png"
+  },
+  {
+    "revision": "25cf204647333eed78230ed7103dc63c",
+    "url": "/img/pet-shop.25cf2046.png"
+  },
+  {
+    "revision": "7bb21e48481602b9427dcdf1831296d1",
+    "url": "/img/pin.7bb21e48.png"
+  },
+  {
+    "revision": "71127a537b940a4507d2294899dc2e20",
+    "url": "/img/prompt-light-webfont.71127a53.svg"
+  },
+  {
+    "revision": "c79926e8562cd96a077a27692523c7f9",
+    "url": "/img/prompt-medium-webfont.c79926e8.svg"
+  },
+  {
+    "revision": "11d1e0e46b10b853db1dfb36b7dedce2",
+    "url": "/img/prompt-regular-webfont.11d1e0e4.svg"
+  },
+  {
+    "revision": "5e3dbd64b33520fd39e8b8929d862e2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "660099d8808b2a57cd02",
+    "url": "/js/app.d856e38a.js"
+  },
+  {
+    "revision": "4f7c443c07289cbf37de",
+    "url": "/js/chunk-vendors.6121ac10.js"
+  },
+  {
+    "revision": "8a5b9dc925c78db9d713e0e66c79fd66",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
